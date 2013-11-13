1da1757ab61106a0cd1cc1d3684ba4d892c2484d
Johan Leitet <johan.leitet@lnu.se>
2013-11-13 20:49:51 +0100

    Demos

---
+++ lectures/E03-Funktioner/demo-map/README.md
@@ -0,0 +1,2 @@
+Fiddle:
+http://jsfiddle.net/gh/get/library/pure/Leitet/Kursmaterial-JavaScript/tree/master/lectures/E02-The-Review/demo
---
+++ lectures/E03-Funktioner/demo-map/demo.js
@@ -0,0 +1,4 @@
+var n = ["Nisse", "Janne", "Ellen", "Lisa"].map(function(name){ return "Namnet " + name; });
+
+console.log(n); //["Namnet Nisse", "Namnet janne", "Namnet Ellen", "Namnet Lisa"]
+
